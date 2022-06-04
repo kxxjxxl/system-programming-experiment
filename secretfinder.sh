291e87d07d12e6ee96ef519833060541c6cae909
Emad Salim <62475696+kxxjxxl@users.noreply.github.com>
2022-06-04 16:04:59 -0230

    Update secretfinder.sh

--- secretfinder.sh
+++ secretfinder.sh
@@ -6,4 +6,4 @@ tar -xvf grep.tar #extracting the tar file
 touch authcode.txt #creating a text file to store the output
 grep "SECRET AUTH CODE" *== >authcode.txt #finding which file has the words and outputting it to a txt file
 rm *== | rm *.tar #removing all files except authcode.txt
-echo "Found the secret auth code! Saved to authcode.txt" #printing confirmation on to screen
+echo "Successfully found the secret auth code and Saved it to authcode.txt" #printing confirmation on to screen
